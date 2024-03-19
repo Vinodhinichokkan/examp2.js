cbe07b60e8f432f1b5f1a83c8a86d48ffaf2f9ef
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-03-19 11:40:29 +0530

    exe

---
+++ examp2.js
@@ -0,0 +1,40 @@
+var eid=600
+var ename="vino"
+var esal=30000
+console.log(`Employee Id:${eid}`)
+console.log(`Employee name:${ename}`)
+console.log(`Employee salary:${esal}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ example1.js
@@ -0,0 +1,5 @@
+var eid=101;
+var ename="vino"
+console.log(eid);
+var ename="abi";
+console.log(ename);
